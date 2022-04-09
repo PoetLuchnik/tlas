6e4fee20eab856f57dccc4e6d53b5b6475ee79b5
PoetLuchnik <artem.pirunow@gmail.com>
2022-04-09 18:40:52 +0300

    add keywords

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 #include<stdio.h>
 
 #include"argreader.h"
+#include"keywords.h"
 
 int main(int n, char** v) {
 	tlas_args_t a;
 	
 	readargs(n, v, &a);
+
+	puts(tlas_kw[TLAS_KW_COMPARE]);
 
 	return 0;
 }
