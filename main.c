89f717060cf2116ecf704a7e4e1f6a42bf3b48a7
PoetLuchnik <artem.pirunow@gmail.com>
2022-04-10 20:01:37 +0300

    temporary remove all files

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-#include"argreader.h"
-#include"keywords.h"
-#include"token.h"
-
 int main(int n, char** v) {
-	tlas_args_t a;
-	readargs(n, v, &a);
 
 	return 0;
 }
